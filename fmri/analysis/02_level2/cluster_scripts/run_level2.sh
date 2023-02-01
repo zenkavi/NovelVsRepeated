ad5b4ced1a5879f9bd63fd6f64c100e362f5729e
Zeynep Enkavi <zeynepenkavi@gmail.com>
2023-02-01 09:26:29 -0800

    debugged missing task name argument

--- fmri/analysis/02_level2/cluster_scripts/run_level2.sh
+++ fmri/analysis/02_level2/cluster_scripts/run_level2.sh
@@ -1,5 +1,5 @@
 set -e
-for modelnum in model1
+for modelnum in model2
 do
   for sign in pos neg
   do
@@ -7,7 +7,10 @@ do
     do
       for session in ses-01 ses-02 ses-03 ses-02_min_ses-01 ses-03_min_ses-01 ses-03_min_ses-02
       do
-        sed -e "s/{MODELNUM}/$modelnum/g" -e "s/{SIGN}/$sign/g" -e "s/{REGNAME}/$regname/g" -e "s/{SESSION}/$session/g" run_level2.batch | sbatch
+        for task in yesNo
+        do
+          sed -e "s/{MODELNUM}/$modelnum/g" -e "s/{SIGN}/$sign/g" -e "s/{REGNAME}/$regname/g" -e "s/{SESSION}/$session/g" -e "s/{TASK}/$task/g" run_level2.batch | sbatch
+        done
       done
     done
   done
